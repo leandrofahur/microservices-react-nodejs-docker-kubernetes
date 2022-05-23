1368ed6b7c6ea848530946199133dcf2ccb145bf
Leandro Machado <fahurleandro@gmail.com>
2022-05-22 22:28:09 -0700

    handling async errors

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import "express-async-errors";
+
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -14,7 +16,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
-app.all("*", () => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 

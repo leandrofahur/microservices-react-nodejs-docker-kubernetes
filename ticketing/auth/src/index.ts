1b299e2c8cf262fcff113e4758d2d5557002ff42
Leandro Machado <fahurleandro@gmail.com>
2022-05-22 17:45:57 -0700

    infra added to the project

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
 app.listen(3000, () => {
   console.log("Server up and running on port 3000!");
 });

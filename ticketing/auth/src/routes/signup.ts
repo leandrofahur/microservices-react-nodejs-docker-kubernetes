b649eec72741521d066543c5dc6e45900d22bb5e
Leandro Machado <fahurleandro@gmail.com>
2022-05-25 07:53:15 -0700

    start to implement signup

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -21,9 +21,16 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    throw new DatabaseConnectionError();
     const { email, password } = req.body;
-    res.send({});
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("Email in use...");
+      return res.send({});
+    }
+
+    const user = User.build({ email, password });
   }
 );
 
